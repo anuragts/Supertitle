a4349113e09f235dea72395f441152086b8e95db
Anurag30112003 <anuragsharma011011@gmail.com>
2023-12-10 04:28:55 +0530

    feat:- analytics #2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <>
-    <Analytics />
-    <html lang="en">
-      <Header>
-      <link href="https://api.fontshare.com/v2/css?f[]=satoshi@400&display=swap" rel="stylesheet"/>
-      </Header>
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <Header>
+          <link href="https://api.fontshare.com/v2/css?f[]=satoshi@400&display=swap" rel="stylesheet" />
+        </Header>
+        <Analytics />
+        <body className={inter.className}>{children}</body>
+      </html>
     </>
   )
 }
